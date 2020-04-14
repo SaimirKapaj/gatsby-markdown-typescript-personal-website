b133700062e59624356d7e2f2fec32c1a51a200f
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-04-14 20:25:01 +0200

    configured gastby remarker plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,27 @@ module.exports = {
         path: `${__dirname}/src/assets/images`
       }
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 768,
+              linkImagesToOriginal: false
+            }
+          }
+        ]
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
