cec1791762fabe906cb23bc293676a3ca2af0ac7
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-04-15 11:32:08 +0200

    created carousel component

---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import Slider from 'react-slick';
+
+import './slick.css';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const settings = {
+  autoplay: true,
+  autoplaySpeed: 5000,
+  speed: 500,
+  infinite: true,
+  dots: true,
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1
+};
+
+/**
+ * Carousel component
+ *
+ * @param {Props} props
+ */
+const Carousel: React.FC<Props> = ({ children }) => (
+  <>
+    <Helmet>
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+      />
+    </Helmet>
+    <Slider {...settings}>{children}</Slider>
+  </>
+);
+
+export default Carousel;
