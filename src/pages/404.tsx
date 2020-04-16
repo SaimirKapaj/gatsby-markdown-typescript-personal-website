798b22e03a84bcced563f5c8f98d2684c62c0bd4
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-04-16 22:25:05 +0200

    styled 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
+import Container from 'components/utils/Container';
+import TitleSection from 'components/utils/TitleSection';
 
 const NotFoundPage: React.FC = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <div className="text-center">
-      <h1 className="text-2xl font-semibold">NOT FOUND</h1>
-      <p className="mt-5">You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <Container section>
+      <TitleSection title="404" subtitle="Page not found" center />
+      <p className="mt-4 text-center w-full">You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Container>
   </Layout>
 );
 
