1907e822255f7076f613795ea4a01c43bf8d1deb
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-04-16 15:48:02 +0200

    added new icons

--- src/components/utils/Icon/index.tsx
+++ src/components/utils/Icon/index.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faLaptopCode, faDrawPolygon, faEdit, faBullhorn } from '@fortawesome/free-solid-svg-icons';
+import {
+  faLaptopCode,
+  faDrawPolygon,
+  faEdit,
+  faBullhorn,
+  faMapMarkerAlt,
+  faPhone,
+  faPaperPlane
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
 
 /**
  * add any additional icon to the library
  */
-library.add(fab, faLaptopCode, faDrawPolygon, faEdit, faEdit, faBullhorn);
+library.add(fab, faLaptopCode, faDrawPolygon, faEdit, faEdit, faBullhorn, faMapMarkerAlt, faPhone, faPaperPlane);
 
 export type IconProps = FontAwesomeIconProps['icon'];
 
