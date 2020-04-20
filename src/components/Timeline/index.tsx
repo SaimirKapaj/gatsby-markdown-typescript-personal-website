54402fc66376dfa58ae78e1b5846c3c6db2749bc
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-04-20 10:53:43 +0200

    created timeline component

---
+++ src/components/Timeline/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import * as Styled from './styles';
+
+interface Props {
+  title: string;
+  subtitle: string;
+  content: React.ReactNode;
+  startDate: string;
+  endDate: string;
+}
+
+const Timeline: React.FC<Props> = ({ title, subtitle, content, startDate, endDate }) => (
+  <Styled.Timeline>
+    <Styled.Point />
+    <Styled.Details>
+      <Styled.Date>
+        {startDate} - {endDate}
+      </Styled.Date>
+      <Styled.Title>{title}</Styled.Title>
+      <Styled.Subtitle>{subtitle}</Styled.Subtitle>
+    </Styled.Details>
+    <Styled.Content>{content}</Styled.Content>
+  </Styled.Timeline>
+);
+
+export default Timeline;
---
+++ src/components/Timeline/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
+
+export const Timeline = styled.div`
+  ${tw`flex flex-col sm:flex-row w-full p-4 relative border-l border-indigo-200`};
+
+  &:last-child {
+    ${tw`pb-0`};
+  }
+`;
+
+export const Details = styled.div`
+  ${tw`w-full sm:w-1/3`};
+`;
+
+export const Content = styled.div`
+  ${tw`w-full sm:w-2/3 mt-4 sm:mt-0`};
+`;
+
+export const Title = styled.div`
+  ${tw`font-semibold mt-3`};
+`;
+
+export const Subtitle = styled.div`
+  ${tw`text-xs`};
+`;
+
+export const Date = styled.div`
+  ${tw`text-xs border  border-teal-400 rounded-full px-2`};
+  width: fit-content;
+`;
+
+export const Point = styled.span`
+  ${tw`w-3 h-3 border border-indigo-200 bg-indigo-100 rounded-full absolute`};
+  left: -6px;
+  top: 20px;
+`;
