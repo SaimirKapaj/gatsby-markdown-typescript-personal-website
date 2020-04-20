838a68eedd2b0ba33375dd8adb54aa9f3d210607
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-04-20 13:02:27 +0200

    removed unused styled componect from contact info

--- src/components/ContactInfo/index.tsx
+++ src/components/ContactInfo/index.tsx
@@ -49,23 +49,21 @@ const ConctactInfo = () => {
   const contacts: Contact[] = allMarkdownRemark.edges;
 
   return (
-    <Styled.ContactInfo>
-      <Container section>
-        <TitleSection title={sectionTitle.title} subtitle={sectionTitle.subtitle} center />
-        {contacts.map((item) => {
-          const {
-            id,
-            frontmatter: { title, icon, content }
-          } = item.node;
+    <Container section>
+      <TitleSection title={sectionTitle.title} subtitle={sectionTitle.subtitle} center />
+      {contacts.map((item) => {
+        const {
+          id,
+          frontmatter: { title, icon, content }
+        } = item.node;
 
-          return (
-            <Styled.ContactInfoItem key={id}>
-              <InfoBlock icon={icon} title={title} content={content} center />
-            </Styled.ContactInfoItem>
-          );
-        })}
-      </Container>
-    </Styled.ContactInfo>
+        return (
+          <Styled.ContactInfoItem key={id}>
+            <InfoBlock icon={icon} title={title} content={content} center />
+          </Styled.ContactInfoItem>
+        );
+      })}
+    </Container>
   );
 };
 
--- src/components/ContactInfo/styles.ts
+++ src/components/ContactInfo/styles.ts
@@ -1,10 +1,6 @@
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
 
-export const ContactInfo = styled.section`
-  ${tw``};
-`;
-
 export const ContactInfoItem = styled.div`
   ${tw`w-full sm:w-1/3`};
 `;
