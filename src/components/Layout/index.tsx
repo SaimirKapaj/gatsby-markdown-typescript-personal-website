0ab1ad3b757ca5c2ec1408aaacc855c5dc10ddad
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-04-24 15:22:34 +0200

    added motion to page transition

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import Header from 'components/Header';
 import Footer from 'components/Footer';
@@ -27,12 +28,21 @@ const Layout: React.FC<Props> = ({ children }) => {
   return (
     <>
       <GlobalStyles />
-      <Styled.Layout>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        {children}
-        <Newsletter />
-        <Footer />
-      </Styled.Layout>
+      <AnimatePresence exitBeforeEnter>
+        <Styled.Layout>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <motion.div
+            initial={{ y: 30, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ delay: 0.2 }}
+          >
+            {children}
+            <Newsletter />
+            <Footer />
+          </motion.div>
+        </Styled.Layout>
+      </AnimatePresence>
     </>
   );
 };
