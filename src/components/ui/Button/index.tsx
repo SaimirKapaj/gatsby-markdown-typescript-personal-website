c74de11da49e9db8d817f5b96bd8a711cb1b9c42
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-04-29 13:46:37 +0200

    extended button type props

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -6,7 +6,7 @@ interface Props extends Styled.StyledProps {
   children: React.ReactNode;
 }
 
-const Button: React.FC<Props> = ({ primary, block, children }) => (
+const Button: React.FC<Props & React.ButtonHTMLAttributes<HTMLButtonElement>> = ({ primary, block, children }) => (
   <Styled.Button primary={primary} block={block} whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
     {children}
   </Styled.Button>
